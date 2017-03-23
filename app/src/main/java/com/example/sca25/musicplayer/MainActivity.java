61e41cc4ccb5b43bfa7ca4f90f1d9e1573352c7f
sca2511 <sca2511@gmail.com>
2017-03-23 03:18:16 -0700

    Basic player complete, with audio file: Big Bang - Fxxk It

--- app/src/main/java/com/example/sca25/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/sca25/musicplayer/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.sca25.musicplayer;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,10 +13,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button playButton = (Button) findViewById(R.id.play);
-        Button pauseButton = (Button) findViewById(R.id.pause);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.a);
+        final Button playButton = (Button) findViewById(R.id.play);
+        final Button pauseButton = (Button) findViewById(R.id.pause);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
+            }
+        });
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.pause();
+            }
+        });
     }
 
-    MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.1);
+
 
 }
